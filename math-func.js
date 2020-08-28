5a5a41915411e00218978270d60baa2edbf1c017
shanksssss <44517115+shanksssss@users.noreply.github.com>
2020-08-28 18:01:35 +0800

    Update math-func.js

--- math-func.js
+++ math-func.js
@@ -3,8 +3,10 @@
 // multiple feature
 const multiply = (x, y) => x * y;
 
-// addition feature
-const addition = (x, y) => x + y;
+// addition feature with brackets on function
+const addition = (x, y) => {
+  return x + y
+};
 
 // console log feature
 console.log('Feature release');
